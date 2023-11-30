35e54361cdf7392b9206fd947e7a784719401b48
DescaMr <drizal07@gmail.com>
2023-12-01 06:02:58 +0700

    bab 6 (proses)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,3 +34,62 @@
         </div>
     </body>
 </html>
+
+<script>
+    const Toast = Swal.mixin({
+        Toast : true,
+        position : 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+    })
+
+    @if(Session::has('message'))
+        var type = "{{Session::get('alert-type')}}";
+
+        switch (type){
+            case 'info':
+                Toast.fire({
+                icon:'info',
+                title: "{{Session::get('message')}}"
+            })
+            break;
+            case 'success':
+                Toast.fire({
+                icon:'success',
+                title: "{{Session::get('message')}}"
+            })
+            break;
+            case 'warning':
+                Toast.fire({
+                icon:'warning',
+                title: "{{Session::get('message')}}"
+            })
+            break;
+            case 'error':
+                Toast.fire({
+                icon:'error',
+                title: "{{Session::get('message')}}"
+            })
+            break;
+            case 'dialog_error':
+                Toast.fire({
+                icon:'error',
+                title: "Oooops",
+                text: "{{Session::get('message')}}"
+                timer: 3000
+            })
+            break;
+        }
+@endif
+    @if ($errors->any())
+    @php $list = null; @endphp
+    @foreach($errors->all() as $errors)
+        @php $list .= '<li>' .$errors. '</li>'; @endphp
+    @endforeach
+    Swal.fire({
+        type: 'error',
+        title: "Ooops",
+        html: "<ul>{!! $list !!}</ul>",
+    })
+    @endif
+</script>
